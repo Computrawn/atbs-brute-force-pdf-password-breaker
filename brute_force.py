55680711a7aa983d252cad87dcf5aa78c33e4294
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-04-26 15:38:55 -0500

    Initial commit.

---
+++ brute_force.py
@@ -0,0 +1,12 @@
+#! python3
+# brute_force.py — An exercise in manipulating PDFs.
+# For more information, see project_details.txt.
+
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="logging.txt",
+    format="%(asctime)s -  %(levelname)s -  %(message)s",
+)
+logging.disable(logging.CRITICAL)  # Note out to enable logging.
